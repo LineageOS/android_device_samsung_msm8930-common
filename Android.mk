711a708dd4f399973e71c9427fc1aa48e288948f
Donald Hoskins <grommish@gmail.com>
2014-11-12 00:34:03 -0500

    msm8930: Add serranolteusc (SCH-R890) to build barrier
    Originally from jason.laskowski@aurora.org
    
    Change-Id: Iee8435b806b115b48634eaec4367a6626ff83844

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@ ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 ifneq ($(filter expressltexx loganreltexx lt02ltespr melius3gxx meliusltexx \
                 serrano3gxx serranodsdd serranodsub serranoltebmc serranoltexx \
-                serranoltespr wilcoxltexx,$(TARGET_DEVICE)),)
+                serranoltespr serranolteusc wilcoxltexx,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
