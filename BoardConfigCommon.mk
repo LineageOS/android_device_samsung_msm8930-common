daeb816118442857b7a6e6d52b9daacf8025554b
Arne Coucheron <arco68@gmail.com>
2014-11-18 00:01:09 +0100

    Increase max partitions to 28
    Change-Id: I8f99933353d132924b5222a825ef1dc4d53b2ec1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,5 +76,5 @@ WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 
 # Vold
-BOARD_VOLD_MAX_PARTITIONS := 24
+BOARD_VOLD_MAX_PARTITIONS := 28
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
