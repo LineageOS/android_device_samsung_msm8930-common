f4df254475f90558ff38987aa98a5269850a29f8
Arne Coucheron <arco68@gmail.com>
2014-11-23 05:59:44 +0100

    Define custom EGL cache sizes
    Change-Id: If89ba2fcf40a32f6c9c495801261dd8c573b7a66

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,7 +64,18 @@ EXTENDED_FONT_FOOTPRINT := true
 # Legacy MMAP for pre-lollipop blobs
 BOARD_USES_LEGACY_MMAP := true
 
+# Use dlmalloc instead of jemalloc for mallocs
 MALLOC_IMPL := dlmalloc
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
 
 # Wifi driver
 BOARD_HAS_QCOM_WLAN              := true
