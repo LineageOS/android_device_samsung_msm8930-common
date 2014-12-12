c1559704613a775d55489d7b9c4b55daa923dcec
Arne Coucheron <arco68@gmail.com>
2014-12-12 23:43:22 +0100

    misc: Create wcd9310 symlinks during build process
    Change-Id: Id2f3794adec1b140620c851ee604aece6738fbf8

--- Android.mk
+++ Android.mk
@@ -31,8 +31,13 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
 	ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-	$(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+		$(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9310; \
+	ln -sf /data/misc/audio/wcd9310_anc.bin \
+		$(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_anc.bin; \
+	ln -sf /data/misc/audio/mbhc.bin \
+		$(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_mbhc.bin)
 endif
 endif
 endif
