2f1beb2ffbe7af35ae78fb1ee74a1576dccbd7ed
Arne Coucheron <arco68@gmail.com>
2014-12-24 13:42:45 +0100

    misc: Create firmware links during build process
    Change-Id: I6a3105f3b8879366d4701e3f2822647c56cc20b6

--- Android.mk
+++ Android.mk
@@ -29,6 +29,84 @@ ifneq ($(filter expressltexx loganreltexx lt02ltespr melius3gxx meliusltexx meli
                 serranoltespr serranolteusc wilcoxltexx,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+MODEM_IMAGES := \
+    modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
+    modem.b06 modem.b07 modem.b08 modem.b09 modem.b10 modem.mdt
+
+MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MODEM_IMAGES)))
+$(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Modem firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(MODEM_SYMLINKS)
+
+MODEM_FW_IMAGES := \
+    modem_fw.b00 modem_fw.b01 modem_fw.b02 modem_fw.b03 modem_fw.b04 \
+    modem_fw.b05 modem_fw.b09 modem_fw.b10 modem_fw.b11 modem_fw.b12 \
+    modem_fw.b13 modem_fw.b15 modem_fw.b16 modem_fw.b17 modem_fw.b18 \
+    modem_fw.b23 modem_fw.b24 modem_fw.b25 modem_fw.b27 modem_fw.b28 \
+    modem_fw.b31 modem_fw.fli modem_fw.mdt
+
+MODEM_FW_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MODEM_FW_IMAGES)))
+$(MODEM_FW_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Modem firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(MODEM_FW_SYMLINKS)
+
+Q6_IMAGES := \
+    q6.b00 q6.b01 q6.b03 q6.b04 q6.b05 q6.b06 q6.mdt
+
+Q6_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(Q6_IMAGES)))
+$(Q6_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Q6 firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(Q6_SYMLINKS)
+
+TZAPPS_IMAGES := \
+    tzapps.b00 tzapps.b01 tzapps.b02 tzapps.b03 tzapps.mdt
+
+TZAPPS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(TZAPPS_IMAGES)))
+$(TZAPPS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "TZAPPS firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(TZAPPS_SYMLINKS)
+
+VIDC_IMAGES := \
+    vidc.b00 vidc.b01 vidc.b02 vidc.b03 vidc.mdt
+
+VIDC_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(VIDC_IMAGES)))
+$(VIDC_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "VIDC firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VIDC_SYMLINKS)
+
+WCNSS_IMAGES := \
+    wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b05 \
+    wcnss.b06 wcnss.mdt
+
+WCNSS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(WCNSS_IMAGES)))
+$(WCNSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_SYMLINKS)
+
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
 	ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
 		$(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
