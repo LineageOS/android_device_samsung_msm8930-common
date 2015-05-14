fd5230ddd2e8e80fe23036052a9fceb5ca94390b
Arne Coucheron <arco68@gmail.com>
2015-05-14 08:29:42 +0200

    sepolicy: Allow debuggerd to read kgsl-3d0
    Change-Id: Ib5b223c591713f01f284b3904996befd974189d7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,7 @@ WIFI_DRIVER_FW_PATH_AP           := "ap"
 BOARD_SEPOLICY_DIRS += device/samsung/msm8930-common/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    debuggerd.te \
     file.te \
     file_contexts \
     init_shell.te \
