fbad2aae61cb3fbc404caf5257c5c281683ce709
Arne Coucheron <arco68@gmail.com>
2017-01-21 05:58:41 +0100

    camera: Extend supported scene modes
    Change-Id: I3a32758e1da4fda76157023cc2f9ff5d2bc5f3ad

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -125,7 +125,7 @@ static char *camera_fixup_getparams(int id, const char *settings)
 
     params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
     params.set(android::CameraParameters::KEY_PREFERRED_PREVIEW_SIZE_FOR_VIDEO, "1280x720");
-    params.set(android::CameraParameters::KEY_SUPPORTED_SCENE_MODES, "auto");
+    params.set(android::CameraParameters::KEY_SUPPORTED_SCENE_MODES, "auto,asd,action,portrait,landscape,night,night-portrait,theatre,beach,snow,sunset,steadyphoto,fireworks,sports,party,candlelight,backlight,flowers,AR");
 
 #ifdef FFC_PICTURE_FIXUP
     if (id == 1) {
