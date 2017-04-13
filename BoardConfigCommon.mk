4745f208784c72f83510a6d6a47a3237cf20e334
Arne Coucheron <arco68@gmail.com>
2017-04-13 12:18:42 +0200

    msm8930-common: Remove custom GLES shader cache sizes
    Change-Id: I1edb06871b7fe973c2185d57abd337ca63a84f67

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,16 +56,6 @@ NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 # Renderscript
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
-# Shader cache config options
-# Maximum size of the  GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
 # Wifi driver
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_WLAN_DEVICE                := qcwcn
