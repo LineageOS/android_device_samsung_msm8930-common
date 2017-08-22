a45e18e1319b71c1263827b8135a3c0c9f83ea8f
Adrian DC <radian.dc@gmail.com>
2017-08-22 11:57:00 +0200

    msm8930-common: Upgrade camera library dependencies for AOSP 8.0.0
    Change-Id: If04e62bbf0905534c03438f95458f0d6c5045ae2
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -44,13 +44,23 @@ ifeq ($(TARGET_ADD_ISO_MODE_HJR),true)
 endif
 
 LOCAL_C_INCLUDES := \
+    framework/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libutils \
+    android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
