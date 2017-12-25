9a8b9e550d1afc0d9538735cc0966727da320a95
Arne Coucheron <arco68@gmail.com>
2017-12-25 14:51:04 +0100

    msm8930-common: Use wlan as module instead of built-in
    Google completely messed up the wifi system in O. With built-in
    driver, wlan never gets properly unloaded when turning off, leading
    to a constant battery drain. Seriously noticeable with hotspot mode.
    Using module (ugh!) this is not a problem.
    
    Change-Id: I208089111b66e7747d5f969019bc5ee4bf9a1568

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,6 +65,8 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 TARGET_PROVIDES_WCNSS_QMI        := true
 TARGET_USES_QCOM_WCNSS_QMI       := true
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
+WIFI_DRIVER_MODULE_PATH          := "/vendor/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 
