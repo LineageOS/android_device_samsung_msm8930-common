5a45732919dae76b583dad708840c133a05360ff
eigo Nonaka <nona@google.com>
2018-01-18 14:28:58 -0800

    msm8930-common: Exclude serif fonts from system.img
    Reduces final image size by about 25MB.
    
    Change-Id: I9e8ae18f2d5fb8259aa62418471a3a70cf1908ed

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,6 +28,9 @@ TARGET_CPU_VARIANT := krait
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
+# Exclude serif fonts for saving system.img size.
+EXCLUDE_SERIF_FONTS := true
+
 # Don't try to build and run all tests by default. Several tests have
 # dependencies on the framework.
 ANDROID_NO_TEST_CHECK := true
