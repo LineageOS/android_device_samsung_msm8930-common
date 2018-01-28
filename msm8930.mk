b1b72eea4a7b7a1f63f9f40a9c4687cf4474bc2c
Arne Coucheron <arco68@gmail.com>
2018-01-28 06:51:00 +0100

    msm8930-common: Add android.hardware.drm@1.0-service
    Change-Id: I4737c0c0765ed77592d81cb21ae890da7db3bece

--- msm8930.mk
+++ msm8930.mk
@@ -99,7 +99,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Sensors
 PRODUCT_PACKAGES += \
