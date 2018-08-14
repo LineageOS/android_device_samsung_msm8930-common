603c86d7761461309d116eb184749817568b50d1
Arne Coucheron <arco68@gmail.com>
2018-08-14 13:12:51 +0200

    [TMP] Disable some stuff
    Change-Id: I40688ab70ef2079c900b91abbc749399d593f71f

--- msm8930.mk
+++ msm8930.mk
@@ -84,7 +84,7 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Camera
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8960
