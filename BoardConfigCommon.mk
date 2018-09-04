818747fff4e2c5ee56326d6ea972d94a0f3414bb
Arne Coucheron <arco68@gmail.com>
2018-09-05 01:49:17 +0200

    msm8930-common: Use smaller font footprint
    Change-Id: Ifc0f841937533463c5ac07a530b30f6fe91d6af7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,6 +28,9 @@ TARGET_CPU_VARIANT := krait
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
+# Fonts
+SMALLER_FONT_FOOTPRINT := true
+
 # Don't try to build and run all tests by default. Several tests have
 # dependencies on the framework.
 ANDROID_NO_TEST_CHECK := true
