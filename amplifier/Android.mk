306f0736f08a1ecedab96ce152a56fbe3effc060
Arne Coucheron <arco68@gmail.com>
2018-10-30 00:58:35 +0100

    msm8930-common: amplifier: Use generated kernel headers
    Change-Id: I5a9bf2db8ce2314f210b2ba40e7c735406f0c85b

--- amplifier/Android.mk
+++ amplifier/Android.mk
@@ -3,12 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += \
-    $(call project-path-for,qcom-audio)/hal \
-    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+    $(call project-path-for,qcom-audio)/hal
 
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_HEADER_LIBRARIES := \
+    libhardware_headers \
+    generated_kernel_headers
 
 LOCAL_MODULE := audio_amplifier.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
