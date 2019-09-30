70baea15f7300c1c56a92825f6a217f3a4f69c43
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    msm8930-common: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    [arco] Also clean mediacodec.policy
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339

--- msm8930.mk
+++ msm8930.mk
@@ -35,8 +35,7 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # WPA supplicant overlay files
 PRODUCT_COPY_FILES += \
