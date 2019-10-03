e295844417cd2b9e1997034d54b12e86d3cab352
Jimmy Chen <jimmycmchen@google.com>
2019-10-03 11:24:23 +0800

    msm8930-common: wifi: Move supplicant service init to wpa_supplicant
    Bug: 138790309
    Test: boot and wifi function is ok
    Change-Id: I35cf3e03566443e20799623045eacd423fd7f5c4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,7 @@ WIFI_DRIVER_MODULE_PATH          := "/vendor/lib/modules/wlan.ko"
 WIFI_DRIVER_MODULE_NAME          := "wlan"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 # Move wcnss_service to vendor
 PRODUCT_VENDOR_MOVE_ENABLED := true
