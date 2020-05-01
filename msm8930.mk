361fbf7e1cc71175110a27903745460cadccfe20
Arne Coucheron <arco68@gmail.com>
2020-05-01 03:29:14 +0200

    msm8930-common: manifest: Switch GNSS HAL to hwbinder
    Change-Id: Ic01f48127a309954453b7b0cf1ea6c276f70a88d

--- msm8930.mk
+++ msm8930.mk
@@ -119,7 +119,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
