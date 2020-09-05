c4d26709c69ff1fac2058c4a7c086d76a03ea196
Kevin F. Haggerty <haggertk@lineageos.org>
2020-09-05 05:28:09 -0600

    msm8930-common: sepolicy: Allow system_app to access storaged via IPC
    avc: denied { call } for comm=4173796E635461736B202333
    scontext=u:r:system_app:s0 tcontext=u:r:storaged:s0 tclass=binder
    permissive=0
    
    Change-Id: I933dcebf2f5960d639ce47be379f62636e4ddd69

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,3 +86,4 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 # Include SE policies
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/common
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
