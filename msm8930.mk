ae1c6478386bcfbe32b7d9e47f9ba0e501a76622
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:18:28 +0800

    msm8930-common: Add prebuilt libprotobuf-cpp-lite from sdk29
    We'll be using it for our libwvdrmengine.so blob.
    
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- msm8930.mk
+++ msm8930.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
