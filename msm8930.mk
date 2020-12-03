8882895ad6bc4b51fc250d238ad6f2c776ec3d70
Arne Coucheron <arco68@gmail.com>
2020-12-03 13:03:50 +0100

    msm8930-common: Use in-process APK for network stack and tethering
    Taken from common go configuration, and the purpose is
    to save some memory.
    
    Change-Id: I32222daf8fa2be1f41a8177b82fa6f2f94e002e7

--- msm8930.mk
+++ msm8930.mk
@@ -162,6 +162,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Do not spin up a separate process for the network stack, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+PRODUCT_PACKAGES += com.android.tethering.inprocess
+
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
